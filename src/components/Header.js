0df8917ec80b163e76513aff8c05d8490ba3212a
ford burgess <fburg6824@gmail.com>
2022-04-18 19:46:07 -0500

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return <div className='header'>
     <div className='header-div-left'>
-      <h1>Trello Clone</h1>
+      <h1>Task Manager</h1>
     </div>
     <div className='header-div-right'>
       <a href="#" className='header-link'>Settings</a>
